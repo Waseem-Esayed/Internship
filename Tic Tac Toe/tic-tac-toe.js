0cd8359ac0b99bd285f5d9e3a284c6000fa409f8
Waseem Esayed <164498143+Waseem-Esayed@users.noreply.github.com>
2024-04-19 15:33:30 +0200

    Update tic-tac-toe.js

--- Tic Tac Toe/tic-tac-toe.js
+++ Tic Tac Toe/tic-tac-toe.js
@@ -31,58 +31,58 @@ let selectedFields1 = [];
 let selectedFields2 = [];
 
 function initField(btn, img) {
-    btn.onclick = function () {
-        if (img.getAttribute("src") !== "images/X.png" && img.getAttribute("src") !== "images/O.png") {
-            if (playerTurn % 2 === 0) {
-                img.setAttribute("src", "images/X.png");
-                selectedFields1.push(img.getAttribute("id"));
-                console.log(selectedFields1);
-                playerTurn++;
-                if (checkIfPlayerWon(selectedFields1)) {
-                    gameResult.textContent = "Player 1 Won!";
-                    noClick();
-                } else if (!noWin()) {
-                    gameResult.textContent = "NO WIN";
-                    noClick();
-                }
-            } else {
-                img.setAttribute("src", "images/O.png");
-                selectedFields2.push(img.getAttribute("id"));
-                console.log(selectedFields2);
-                playerTurn++;
-                if (checkIfPlayerWon(selectedFields2)) {
-                    gameResult.textContent = "Player 2 Won!";
-                    noClick();
-                } else if (!noWin()) {
-                    gameResult.textContent = "NO WIN";
-                    noClick();
-                }
-            }
+  btn.onclick = function() {
+    if (img.getAttribute("src") !== "images/X.png" && img.getAttribute("src") !== "images/O.png") {
+      if (playerTurn % 2 === 0) {
+        img.setAttribute("src", "images/X.png");
+        selectedFields1.push(img.getAttribute("id"));
+        console.log(selectedFields1);
+        playerTurn++;
+        if (checkIfPlayerWon(selectedFields1)) {
+          gameResult.textContent = "Player 1 Won!";
+          noClick();
+        } else if (!noWin()) {
+          gameResult.textContent = "NO WIN";
+          noClick();
         }
-    };
+      } else {
+        img.setAttribute("src", "images/O.png");
+        selectedFields2.push(img.getAttribute("id"));
+        console.log(selectedFields2);
+        playerTurn++;
+        if (checkIfPlayerWon(selectedFields2)) {
+          gameResult.textContent = "Player 2 Won!";
+          noClick();
+        } else if (!noWin()) {
+          gameResult.textContent = "NO WIN";
+          noClick();
+        }
+      }
+    }
+  };
 }
 
 function noWin() {
-    return selectedFields1.length + selectedFields2.length < 9;
+  return selectedFields1.length + selectedFields2.length < 9;
 }
 
 function checkIfPlayerWon(selectedFields) {
-    return (selectedFields.includes("img1") && selectedFields.includes("img2") && selectedFields.includes("img3"))
-        || (selectedFields.includes("img4") && selectedFields.includes("img5") && selectedFields.includes("img6"))
-        || (selectedFields.includes("img7") && selectedFields.includes("img8") && selectedFields.includes("img9"))
-        || (selectedFields.includes("img1") && selectedFields.includes("img4") && selectedFields.includes("img7"))
-        || (selectedFields.includes("img2") && selectedFields.includes("img5") && selectedFields.includes("img8"))
-        || (selectedFields.includes("img3") && selectedFields.includes("img6") && selectedFields.includes("img9"))
-        || (selectedFields.includes("img1") && selectedFields.includes("img5") && selectedFields.includes("img9"))
-        || (selectedFields.includes("img3") && selectedFields.includes("img5") && selectedFields.includes("img7"));
+  return (selectedFields.includes("img1") && selectedFields.includes("img2") && selectedFields.includes("img3")) ||
+    (selectedFields.includes("img4") && selectedFields.includes("img5") && selectedFields.includes("img6")) ||
+    (selectedFields.includes("img7") && selectedFields.includes("img8") && selectedFields.includes("img9")) ||
+    (selectedFields.includes("img1") && selectedFields.includes("img4") && selectedFields.includes("img7")) ||
+    (selectedFields.includes("img2") && selectedFields.includes("img5") && selectedFields.includes("img8")) ||
+    (selectedFields.includes("img3") && selectedFields.includes("img6") && selectedFields.includes("img9")) ||
+    (selectedFields.includes("img1") && selectedFields.includes("img5") && selectedFields.includes("img9")) ||
+    (selectedFields.includes("img3") && selectedFields.includes("img5") && selectedFields.includes("img7"));
 }
 
-btnReset.onclick = function () {
-    if (checkIfPlayerWon(selectedFields1)) || (checkIfPlayerWon(selectedFields2)) {
-        for (let i = 0; i < 9; i++) {
-        contentImg[i].setAttribute("src", "");
-        contentBtn[i].onclick = null;
-        initField(contentBtn[i], contentImg[i]);
+btnReset.onclick = function() {
+  if (checkIfPlayerWon(selectedFields1) || checkIfPlayerWon(selectedFields2)) {
+    for (let i = 0; i < 9; i++) {
+      contentImg[i].setAttribute("src", "");
+      contentBtn[i].onclick = null;
+      initField(contentBtn[i], contentImg[i]);
     }
     playerTurn = 1;
 
@@ -91,27 +91,29 @@ btnReset.onclick = function () {
 
     gameResult.textContent = "";
     playerTurn = Math.round(Math.random() * (1 + 10) - 10);
-    }
+  }
 };
 
+
+
 for (let i = 0; i < 9; i++) {
-    initField(contentBtn[i], contentImg[i]);
+  initField(contentBtn[i], contentImg[i]);
 }
 
 function noClick() {
-    getBtn(btn1);
-    getBtn(btn2);
-    getBtn(btn3);
-    getBtn(btn4);
-    getBtn(btn5);
-    getBtn(btn6);
-    getBtn(btn7);
-    getBtn(btn8);
-    getBtn(btn9);
+  getBtn(btn1);
+  getBtn(btn2);
+  getBtn(btn3);
+  getBtn(btn4);
+  getBtn(btn5);
+  getBtn(btn6);
+  getBtn(btn7);
+  getBtn(btn8);
+  getBtn(btn9);
 }
 
 function getBtn(btn, img) {
-    btn.onclick = function () {
-        img.setAttribute("src", img.getAttribute("src"));
-    };
+  btn.onclick = function() {
+    img.setAttribute("src", img.getAttribute("src"));
+  };
 }
