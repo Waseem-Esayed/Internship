84c212fcdab489460f9b734e3bfee35f487c8018
Waseem Esayed <164498143+Waseem-Esayed@users.noreply.github.com>
2024-04-21 17:01:41 +0200

    Update Color Flipper.js

--- Color Flipper/Color Flipper.js
+++ Color Flipper/Color Flipper.js
@@ -3,13 +3,28 @@ body = document.getElementById("body");
 green = document.getElementById("green");
 red = document.getElementById("red");
 blue = document.getElementById("blue");
+random = document.getElementById("random");
+
+let redCol;
+let greenCol;
+let blueCol;
 
 changeColor(green);
 changeColor(red);
 changeColor(blue);
 
+random.onclick = randomColor;
+
 function changeColor(color) {
-    color.onclick = function() {
+    color.onclick = function () {
         body.style.backgroundColor = color.id;
     }
+}
+
+function randomColor() {
+    redCol = Math.round(Math.random() * 255);
+    greenCol = Math.round(Math.random() * 255);
+    blueCol = Math.round(Math.random() * 255);
+
+    body.style.backgroundColor = `rgb(${redCol}, ${greenCol}, ${blueCol})`;
 }
