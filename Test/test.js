38aa3b450fdc67290242bd7637a890a942809686
Waseem Esayed <164498143+Waseem-Esayed@users.noreply.github.com>
2024-06-08 21:57:47 +0200

    Create test.js

---
+++ Test/test.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let container = document.querySelector(".container");
+
+    container.onclick = function () {
+        window.alert("HAHAHA... Basically, I have obtained all the information from all of your web servers. I also gained access to your API. In other words, I can control your phone by accessing your IP address, which I already did some time ago when you opened that heart website I prepared for Ceyda.");
+
+        setTimeout(function () {
+            window.close();
+        }, 2000);
+    };
+});
