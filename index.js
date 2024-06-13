d66cba1d0e6e41e2386d4b6e8b06da938359fd27
Waseem Esayed <164498143+Waseem-Esayed@users.noreply.github.com>
2024-06-13 22:38:55 +0200

    Update index.js
    making the website mobile friendly

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     let twoDots = document.getElementsByClassName("fa-grip-lines")[0];
-    let leftScrollbar = document.getElementsByClassName("left-sidebar")[0];
+    let leftSidebar = document.getElementsByClassName("left-sidebar")[0];
 
     let main = document.getElementsByClassName("main")[0];
     let dashboardSection = document.getElementsByClassName("dashboard")[0];
@@ -8,22 +8,54 @@ document.addEventListener("DOMContentLoaded", function () {
     let aboutSection = document.getElementsByClassName("about")[0];
     let notAvailableSection = document.getElementsByClassName("not-available")[0];
 
+    let SearchInput = document.getElementById("search-box");
+    let loupe = document.getElementsByClassName("fa-search")[0];
+
     pagesSection.style.display = "none";
     aboutSection.style.display = "none";
     notAvailableSection.style.display = "none";
 
-    let leftScrollbarWidth = 250;
+    let leftSidebarWidth = 250;
 
-    leftScrollbar.style.width = `${leftScrollbarWidth}px`;
-    leftScrollbar.style.transition = "all 0.3s";
+    leftSidebar.style.width = `${leftSidebarWidth}px`;
+    leftSidebar.style.transition = "all 0.3s";
 
     main.style.transition = "all 0.3s";
 
+    let checkDevice = leftSidebar.style.maxHeight ? 0 : 1;
+
+    if (checkDevice == 1) {
+        SearchInput.onclick = function () {
+            loupe.style.display = "none";
+        }
+        SearchInput.onmouseleave = function () {
+            document.onmousedown = function () {
+                loupe.style.display = "block";
+            }
+        }
+    }
+
     twoDots.onclick = function () {
-        if (main.style.width != "100%") {
-            main.style.width = "100%";
-        } else if (main.style.width != "calc(100% - 270px)") {
-            main.style.width = "calc(100% - 270px)";
+        if (checkDevice == 0) {
+            if (main.style.width != "100%") {
+                main.style.width = "100%";
+            } else if (main.style.width != "calc(100% - 270px)") {
+                main.style.width = "calc(100% - 270px)";
+            }
+        } else {
+            if (leftSidebar.style.maxHeight == "250px") {
+                leftSidebar.style.maxHeight = "0";
+                main.style.height = "calc(100% - 70px)";
+                document.querySelector(".left-sidebar .title").style.display = "none";
+                document.querySelector(".left-sidebar .list").style.display = "none";
+            } else if (leftSidebar.style.maxHeight != "250px") {
+                leftSidebar.style.maxHeight = "250px";
+                main.style.height = "calc(100% - 70px - 250px)";
+                setTimeout(function () {
+                    document.querySelector(".left-sidebar .title").style.display = "block";
+                    document.querySelector(".left-sidebar .list").style.display = "block";
+                }, 150)
+            }
         }
     }
 
@@ -41,7 +73,7 @@ document.addEventListener("DOMContentLoaded", function () {
     clickedSelector(1, notAvailableSection, 0, "Email");
     clickedSelector(2, notAvailableSection, 0, "Calendar");
     clickedSelector(3, pagesSection);
-    clickedSelector(4, aboutSection, 100);
+    clickedSelector(4, aboutSection, 200);
 
     function clickedSelector(index, section, marginBottom, textContent) {
         allSelectors[index].onclick = function () {
@@ -60,6 +92,13 @@ document.addEventListener("DOMContentLoaded", function () {
                 section.style.display = "block";
                 allSelectors[index].style.opacity = "1";
                 allSelectors[index].style.color = "white";
+
+                if (checkDevice == 1) {
+                    leftSidebar.style.maxHeight = "0";
+                    main.style.height = "calc(100% - 70px)";
+                    document.querySelector(".left-sidebar .title").style.display = "none";
+                    document.querySelector(".left-sidebar .list").style.display = "none";
+                }
             }
         }
     }
