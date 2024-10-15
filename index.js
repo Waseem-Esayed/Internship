50021f2a9d30ed11269893130f05a734f363f352
Waseem Esayed <164498143+Waseem-Esayed@users.noreply.github.com>
2024-10-15 20:13:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     let navElements = document.querySelectorAll('header .nav_elements_box ul li a');
     let navLine = document.getElementById('home-line');
+    let main = document.querySelector("body > main");
+    let firstPage = document.querySelector("body > main > div.first-page");
+    let secondPage = document.querySelector("body > main > div.second-page");
+    let capImage = document.getElementById('cap-image');
+    let firstPageContentBox = document.querySelector('main .first-page .container .content-box');
+    let firstPageCircleContainer = document.querySelector('main .first-page .container .circle-container');
+    let secondPageLeftSide = document.querySelector('main .second-page .left-side');
+    let secondPageRightSide = document.querySelector('main .second-page .right-side');
+
+    //main.style.height = firstPage.clientHeight + secondPage.clientHeight + 'px';
 
     navElements.forEach(function (element) {
         element.style.color = 'white';
@@ -18,13 +28,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     })
 
-    let capImage = document.getElementById('cap-image');
-    let firstPageContentBox = document.querySelector('main .first-page .container .content-box');
-    let firstPageCircleContainer = document.querySelector('main .first-page .container .circle-container');
-
-    let secondPageLeftSide = document.querySelector('main .second-page .left-side');
-    let secondPageRightSide = document.querySelector('main .second-page .right-side');
-
     capImage.style.position = 'relative';
     firstPageContentBox.style.position = 'relative';
     firstPageCircleContainer.style.position = 'relative';
@@ -37,13 +40,41 @@ document.addEventListener('DOMContentLoaded', function () {
     firstPageContentBox.style.opacity = 1;
     firstPageCircleContainer.style.opacity = 1;
 
-    window.onscroll = function () {
-        if (scrollY > 275) {
-            secondPageLeftSide.style.right = 0;
-            secondPageRightSide.style.left = 0;
+    if (window.matchMedia("(min-width: 769px)").matches) {
+        // Der Bildschirm ist größer als 768px
+        window.onscroll = function () {
+            if (window.scrollY <= window.innerHeight) {
+                secondPageLeftSide.style.right = (0 - (window.scrollY - 20)) + window.innerHeight + 'px';
+                secondPageRightSide.style.left = (0 - (window.scrollY - 20)) + window.innerHeight + 'px';
 
-            secondPageLeftSide.style.opacity = 1;
-            secondPageRightSide.style.opacity = 1;
+                secondPageLeftSide.style.opacity = 1;
+                secondPageRightSide.style.opacity = 1;
+            } else {
+                secondPageLeftSide.style.right = 0;
+                secondPageRightSide.style.left = 0;
+            }
+        }
+    } else {
+        window.onscroll = function () {
+            if (window.scrollY - window.innerHeight + 1000 <= window.innerHeight) {
+                secondPageLeftSide.style.right = (0 - (window.scrollY - window.innerHeight + 1000 - 20)) + window.innerHeight + 'px';
+                secondPageLeftSide.style.opacity = 1;
+            } else {
+                secondPageLeftSide.style.right = 0;
+            }
+            if (window.scrollY - window.innerHeight + 500 <= window.innerHeight) {
+                secondPageRightSide.style.left = (0 - (window.scrollY - window.innerHeight + 500 - 20)) + window.innerHeight + 'px';
+                secondPageRightSide.style.opacity = 1;
+            } else {
+                secondPageRightSide.style.left = 0;
+            }
         }
     }
 })
+/*
+window.alert(scrollY);
+window.alert(window.innerHeight);
+
+let secondPageRightSide = document.querySelector('main .second-page .right-side');
+console.log(secondPageRightSide.style.opacity);
+*/
