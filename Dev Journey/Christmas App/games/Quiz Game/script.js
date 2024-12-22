9ba00ca96f009a74ce2fb2fd001b30108ddd7238
Waseem Esayed <164498143+Waseem-Esayed@users.noreply.github.com>
2024-12-22 20:51:05 +0100

    Update script.js

--- Dev Journey/Christmas App/games/Quiz Game/script.js
+++ Dev Journey/Christmas App/games/Quiz Game/script.js
@@ -1,313 +1,57 @@
-let DOMcontentContainer = document.getElementsByClassName("container")[0];
-let DOMtimer = document.getElementById("time-counter");
-let DOMquestion = document.getElementById("question");
-let DOMtotalQuestions = document.getElementById("total-questions");
-let DOMfirstOpt = document.getElementById("first-option");
-let DOMsecondOpt = document.getElementById("second-option");
-let DOMthirdOpt = document.getElementById("third-option");
-let DOMfourthOpt = document.getElementById("fourth-option");
-let DOMcurrentQueNumbs = document.getElementsByClassName(
-  "current-question-number"
-);
-let DOMnextBtn = document.getElementById("next-btn");
+let demos = document.querySelectorAll(".demo");
+let shadows = document.querySelectorAll(".shadow");
+let features = document.querySelectorAll(".feature");
 
-let currentQueNumb = 1;
-let firstRandomIndex;
-let secondRandomIndex;
+demos.forEach((element) => {
+  element.style.height =
+    parseFloat(window.getComputedStyle(element).width) - 70 + "px";
+  window.onresize = function () {
+    element.style.height =
+      parseFloat(window.getComputedStyle(element).width) - 70 + "px";
+  };
+  element.onmouseleave = function () {
+    element.classList.add("leave");
+  };
+  element.onmouseenter = function () {
+    element.classList.remove("leave");
+  };
+});
 
-let timeInterval;
-let time;
-let skipped = false;
+shadows.forEach((element) => {
+  element.style.height =
+    Math.sqrt(
+      Math.pow(
+        parseFloat(
+          window.getComputedStyle(document.getElementsByClassName("demo")[0])
+            .width
+        ),
+        2
+      ) +
+        Math.pow(
+          parseFloat(
+            window.getComputedStyle(document.getElementsByClassName("demo")[0])
+              .height
+          ),
+          2
+        )
+    ) + "px";
+});
 
-let clickedElement = false;
-
-let questionsList = [
-  ["What is the name of Santa's reindeer with a red nose?", "390px"],
-  ["Which plant is traditionally used to decorate during Christmas?", "420px"],
-  [
-    "What is the title of the famous Christmas song that starts with 'Silent night, holy night'?",
-    "420px",
-  ],
-  ["What do people traditionally put on top of a Christmas tree?", "420px"],
-  ["What is the traditional color scheme of Christmas?", "390px"],
-  [
-    "In which month does winter officially start in the Northern Hemisphere?",
-    "420px",
-  ],
-  [
-    "What is the name of the festival of lights celebrated in winter by Jewish people?",
-    "420px",
-  ],
-  ["What is Frosty the Snowman made of?", "390px"],
-  [
-    "Which beverage is commonly associated with Christmas, made of eggs, milk, sugar, and spices?",
-    "450px",
-  ],
-  ["What is the name of the holiday celebrated on December 25th?", "420px"],
-];
-
-let answersList = [
-  [
-    ["Rudolph", true],
-    ["Blitzen", false],
-    ["Comet", false],
-    ["Dasher", false],
-  ],
-  [
-    ["Mistletoe", true],
-    ["Rose", false],
-    ["Tulip", false],
-    ["Daffodil", false],
-  ],
-  [
-    ["Silent Night", true],
-    ["Jingle Bells", false],
-    ["Deck the Halls", false],
-    ["O Holy Night", false],
-  ],
-  [
-    ["Star", true],
-    ["Candle", false],
-    ["Bell", false],
-    ["Ornament", false],
-  ],
-  [
-    ["Red and Green", true],
-    ["Blue and White", false],
-    ["Yellow and Gold", false],
-    ["Purple and Silver", false],
-  ],
-  [
-    ["December", true],
-    ["November", false],
-    ["January", false],
-    ["October", false],
-  ],
-  [
-    ["Hanukkah", true],
-    ["Kwanzaa", false],
-    ["Diwali", false],
-    ["Ramadan", false],
-  ],
-  [
-    ["Snow", true],
-    ["Paper", false],
-    ["Plastic", false],
-    ["Wood", false],
-  ],
-  [
-    ["Eggnog", true],
-    ["Hot Chocolate", false],
-    ["Mulled Wine", false],
-    ["Apple Cider", false],
-  ],
-  [
-    ["Christmas", true],
-    ["Easter", false],
-    ["Halloween", false],
-    ["Thanksgiving", false],
-  ],
-];
-
-DOMtotalQuestions.textContent = questionsList.length;
-
-DOMcontentContainer.style.top =
-  (window.innerHeight - DOMcontentContainer.offsetHeight) / 2 + "px";
-
-displayQuotes();
-resetTime();
-changeQuestNumb();
-DOMnextBtn.onclick = function () {
-  if (!DOMnextBtn.classList.contains("disabled")) {
-    if (DOMnextBtn.textContent === "Next Que") {
-      skipped = true;
-      displayQuotes();
-      DOMnextBtn.classList.add("disabled");
-      document.getElementsByTagName("header")[0].classList = "";
-      Array.from(document.getElementsByClassName("before-solution")).forEach(
-        (item) => {
-          item.style.backgroundColor = "rgba(0, 123, 255, 0.1)";
-          item.style.borderColor = "rgba(0, 123, 255, 0.3)";
-        }
-      );
-      clickedElement = false;
-    } else {
-      location.reload();
-    }
-  }
-};
-
-function displayQuotes() {
-  if (questionsList.length > 0) {
-    firstRandomIndex = Math.floor(Math.random() * questionsList.length);
-    DOMquestion.textContent = questionsList[firstRandomIndex][0];
-    questionsList[firstRandomIndex].splice(0, 1);
-    DOMcontentContainer.style.height = questionsList[firstRandomIndex][0];
-    questionsList.splice(firstRandomIndex, 1);
-
-    DOMfirstOpt.classList = "";
-    DOMsecondOpt.classList = "";
-    DOMthirdOpt.classList = "";
-    DOMfourthOpt.classList = "";
-    DOMfirstOpt.classList.add("before-solution");
-    DOMsecondOpt.classList.add("before-solution");
-    DOMthirdOpt.classList.add("before-solution");
-    DOMfourthOpt.classList.add("before-solution");
-
-    secondRandomIndex = Math.floor(
-      Math.random() * answersList[firstRandomIndex].length
-    );
-    DOMfirstOpt.textContent =
-      answersList[firstRandomIndex][secondRandomIndex][0];
-    DOMfirstOpt.classList.add(
-      answersList[firstRandomIndex][secondRandomIndex][1]
-    );
-    answersList[firstRandomIndex].splice(secondRandomIndex, 1);
-
-    secondRandomIndex = Math.floor(
-      Math.random() * answersList[firstRandomIndex].length
-    );
-    DOMsecondOpt.textContent =
-      answersList[firstRandomIndex][secondRandomIndex][0];
-    DOMsecondOpt.classList.add(
-      answersList[firstRandomIndex][secondRandomIndex][1]
-    );
-    answersList[firstRandomIndex].splice(secondRandomIndex, 1);
-
-    secondRandomIndex = Math.floor(
-      Math.random() * answersList[firstRandomIndex].length
-    );
-    DOMthirdOpt.textContent =
-      answersList[firstRandomIndex][secondRandomIndex][0];
-    DOMthirdOpt.classList.add(
-      answersList[firstRandomIndex][secondRandomIndex][1]
-    );
-    answersList[firstRandomIndex].splice(secondRandomIndex, 1);
-
-    secondRandomIndex = Math.floor(
-      Math.random() * answersList[firstRandomIndex].length
-    );
-    DOMfourthOpt.textContent =
-      answersList[firstRandomIndex][secondRandomIndex][0];
-    DOMfourthOpt.classList.add(
-      answersList[firstRandomIndex][secondRandomIndex][1]
-    );
-    answersList[firstRandomIndex].splice(secondRandomIndex, 1);
-
-    answersList.splice(firstRandomIndex, 1);
-
-    if (skipped) {
-      changeQuestNumb();
-      resetTime();
-      document.getElementsByTagName("header")[0].classList.remove("game-over");
-      DOMnextBtn.classList.add("disabled");
-      setTimeout(() => {
-        skipped = false;
-      }, 4000);
-    }
-  }
-}
-
-function changeQuestNumb() {
-  Array.from(DOMcurrentQueNumbs).forEach((element) => {
-    element.textContent = currentQueNumb + ".";
-    if (element.id === "question-number") element.textContent = currentQueNumb;
+function setFeatureWidth() {
+  features.forEach((element) => {
+    element.style.height =
+      parseFloat(
+        window.getComputedStyle(document.getElementsByClassName("feature")[0])
+          .width
+      ) *
+        0.7 +
+      "px";
   });
-  currentQueNumb++;
 }
 
-function resetTime() {
-  time = 15;
-  DOMtimer.textContent = "15";
-  if (timeInterval) clearInterval(timeInterval);
-  timeInterval = setInterval(() => {
-    time--;
-    time < 10
-      ? (DOMtimer.textContent = "0" + time)
-      : (DOMtimer.textContent = time);
-    if (time === 0 && questionsList.length > 0) {
-      clickedElement = true;
-      Array.from(document.getElementsByClassName("before-solution")).forEach(
-        (item) => {
-          item.classList.remove("before-solution");
-          if (item.classList.contains("true")) {
-            item.style.backgroundColor = "rgb(250, 215, 221)";
-            item.style.borderColor = "rgb(250, 215, 221)";
-            let wrongIcon = document.createElement("i");
-            wrongIcon.classList.add("fa-regular", "fa-circle-xmark");
-            wrongIcon.style.color = "rgb(250, 65, 71)";
-            item.appendChild(wrongIcon);
-          }
-        }
-      );
-      DOMnextBtn.classList.remove("disabled");
-      clearInterval(timeInterval);
-      document.getElementsByTagName("header")[0].classList.add("game-over");
-    } else if (time === 0 && questionsList.length === 0) {
-      DOMnextBtn.textContent = "Try Again";
-      DOMnextBtn.classList.remove("disabled");
-      clearInterval(timeInterval);
-      clickedElement = true;
-      document.getElementsByTagName("header")[0].classList.add("game-over");
-      Array.from(document.getElementsByClassName("before-solution")).forEach(
-        (item) => {
-          item.classList.remove("before-solution");
-        }
-      );
-    }
-  }, 1000);
-}
+setFeatureWidth();
 
-clickingElement();
-
-function clickingElement() {
-  Array.from(document.getElementsByClassName("before-solution")).forEach(
-    (element) => {
-      element.onclick = function () {
-        if (!clickedElement) {
-          if (questionsList.length > 0) {
-            DOMnextBtn.classList.remove("disabled");
-          } else {
-            DOMnextBtn.textContent = "Try Again";
-            DOMnextBtn.classList.remove("disabled");
-          }
-          if (element.classList.contains("true")) {
-            document
-              .getElementsByTagName("header")[0]
-              .classList.add("true-answer");
-          } else {
-            document
-              .getElementsByTagName("header")[0]
-              .classList.add("game-over");
-          }
-          Array.from(
-            document.getElementsByClassName("before-solution")
-          ).forEach((item) => {
-            if (item.classList.contains("true")) {
-              item.style.backgroundColor = "rgb(213, 236, 216)";
-              item.style.borderColor = "rgb(213, 236, 216)";
-              let correctIcon = document.createElement("i");
-              correctIcon.classList.add("fa-regular", "fa-circle-check");
-              correctIcon.style.color = "green";
-              item.appendChild(correctIcon);
-            }
-            item.classList.remove("before-solution");
-          });
-          if (!element.classList.contains("true")) {
-            element.style.backgroundColor = "rgb(250, 215, 221)";
-            element.style.borderColor = "rgb(250, 215, 221)";
-            let wrongIcon = document.createElement("i");
-            wrongIcon.classList.add("fa-regular", "fa-circle-xmark");
-            wrongIcon.style.color = "rgb(250, 65, 71)";
-            element.appendChild(wrongIcon);
-          }
-          clickedElement = true;
-          clearInterval(timeInterval);
-        }
-      };
-    }
-  );
-}
+window.onresize = setFeatureWidth;
 
 function createSnowflake() {
   const snowflake = document.createElement("i");
@@ -325,3 +69,21 @@ function createSnowflake() {
 }
 
 setInterval(createSnowflake, 200);
+
+function countdown() {
+  const christmasDate = new Date("December 25, 2024 00:00:00").getTime();
+  const now = new Date().getTime();
+  const diff = christmasDate - now;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+}
+
+setInterval(countdown, 1000);
